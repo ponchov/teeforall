d8d3eac121de3bc6f37d24e6a7e98f22ff537c4b
Jimmy Sole <jimmysole@gmail.com>
2015-11-14 05:30:54 -0500

    continue work on login module

--- module/Login/src/Login/Controller/LoginController.php
+++ module/Login/src/Login/Controller/LoginController.php
@@ -8,13 +8,44 @@
 namespace Login\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Login\Form\LoginForm;
+use Login\Model\Login;
+
 
 class LoginController extends AbstractActionController
 {
     public function indexAction()
     {
-        return array();
+        $form = new LoginForm();
+        
+        $form->get('submit')->setValue('Login');
+        
+        $messages = null;
+        
+        // gets the form method request (usually post)
+        $request = $this->getRequest();
+        
+        // check to see if the request was a POST form request
+        if ($request->isPost()) {
+            // good to go
+            // filter the form values now
+            $form_filters = new Login();
+            
+            $form->setInputFilter($form_filters->getInputFilter());
+            
+            // set the form data to hold all the values supplied by the form
+            // via $request->getPost()
+            $form->setData($request->getPost());
+            
+            // now we will see if the form is valid
+            // we check if it is valid by the LoginForm class we created
+            if ($form->isValid()) {
+                // it is valid
+                // assign $data to hold all the form data in an assoc. array 
+                // e.g. $data = $form->getData(); $data['name'];
+                $data = $form->getData();
+                
+            }
+        }
     }
-
-    
 }
