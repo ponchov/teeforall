31b0a774a6194ee41b935f30eb97e57b1aa3b3b4
Jimmy Sole <jimmysole@gmail.com>
2015-11-13 22:29:32 -0500

    removed unnecessary view, took out the action for it from the controller
    added the form and model folders and started work on the AdminModel

--- module/Admin/src/Admin/Controller/AdminController.php
+++ module/Admin/src/Admin/Controller/AdminController.php
@@ -1,10 +1,8 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/Admin for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+/*
+ * @author Jimmy
+ * 
+ * Admin Page Controller
  */
 
 namespace Admin\Controller;
@@ -18,10 +16,5 @@ class AdminController extends AbstractActionController
         return array();
     }
 
-    public function fooAction()
-    {
-        // This shows the :controller and :action parameters in default route
-        // are working when you browse to /admin/admin/foo
-        return array();
-    }
+    
 }
---
+++ module/Admin/src/Admin/Model/AdminModel.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * @author Jimmy
+ * 
+ * Class that holds all the admin functionality for use by the admin
+ */
+namespace Admin\Model;
+
+use Zend\Db\TableGateway\TableGateway;
+
+class AdminModel
+{
+    protected $table_gateway;
+    
+    
+    public function __construct(TableGateway $gateway)
+    {
+        // only assign the class TableGateway to $this->table_gateway
+        // if $gateway is a valid instanceof TableGateway
+        $this->table_gateway = $gateway instanceof TableGateway ? $gateway : null;    
+    }
+}
