2b4dda127e35f1b58d6c14f8b3260397bf5f5024
Jimmy Sole <jimmysole@gmail.com>
2015-11-18 05:41:31 -0500

    implemented save email template method

--- module/Admin/src/Admin/Model/EmailTemplatesModel.php
+++ module/Admin/src/Admin/Model/EmailTemplatesModel.php
@@ -7,6 +7,7 @@
 namespace Admin\Model;
 
 use Zend\Db\TableGateway\TableGateway;
+use Error\ErrorHandler;
 
 
 class EmailTemplatesModel
@@ -23,6 +24,34 @@ class EmailTemplatesModel
     
     public function saveEmailTemplate(EmailTemplates $email_tpls)
     {
+        // first let's check if the template already exists
+        // if it does, no need to overwrite it
+        // one would use the modifyEmailTemplate method instead
+        // this method is solely for inserting new email templates
+        $select = $this->table_gateway->select(array('email_subject' => $email_tpls->email_subject));
         
+        $row = $select->current();
+        
+        if (!$row) {
+            // record was not found
+            // insert the template
+            try {
+                $insert = $this->table_gateway->insert(array(
+                    'email_subject' => $email_tpls->email_subject,
+                    'email_body'    => $email_tpls->email_body,
+                ));
+            
+                if ($insert > 0) {
+                    return true;
+                }
+            } catch (\ErrorException $e) {
+                // log the message to the error file
+                ErrorHandler::errorWriter($e->getMessage());
+            }
+        } else {
+            // looks like a row was found
+            // return false
+            return false;
+        }
     }
 }
