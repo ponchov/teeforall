9e2c75d58c63d51110d7cad07c5e42706e654999
Jimmy Sole <jimmysole@gmail.com>
2015-11-20 16:48:20 -0500

    campaign controller

---
+++ module/Campaign/src/Campaign/Controller/CampaignController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Campaign\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class CampaignController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+
+}
